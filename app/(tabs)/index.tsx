5dfc34330c5d7d13581284850e1759d27c05bc99
Grace Nguyen <thuchanh2904@gmail.com>
2026-04-29 10:37:06 +1000

    Homepage

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,37 +1,39 @@
 import "@/global.css";
-import { Link } from "expo-router";
+import dayjs from "dayjs";
 import { styled } from "nativewind";
-import { Text } from "react-native";
+import { Image, Text, View } from "react-native";
 import { SafeAreaView as RNSAV } from "react-native-safe-area-context";
+import { HOME_BALANCE, HOME_USER } from "../constants/data";
+import { icons } from "../constants/icons";
+import images from "../constants/images";
+import { formatCurrency } from "../lib/utils";
 
 const SafeAreaView = styled(RNSAV);
 
 export default function App() {
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-      <Text className="text-5xl font-sans-extrabold">Home</Text>
+      <View className="home-header">
+        <View className="home-user">
+          <Image source={images.avatar} className="home-avatar" />
+          <Text className="home-user-name">{HOME_USER.name}</Text>
+        </View>
 
-      <Link
-        href="/onboarding"
-        className="mt-4 font-sans-bold p-4 rounded bg-primary text-white"
-      >
-        {" "}
-        Go to onboarding{" "}
-      </Link>
-      <Link
-        href="/(auth)/sign-in"
-        className="mt-4 font-sans-bold p-4 rounded bg-primary text-white"
-      >
-        {" "}
-        Go to Sign in
-      </Link>
-      <Link
-        href="/(auth)/sign-up"
-        className="mt-4 font-sans-bold p-4 rounded bg-primary text-white"
-      >
-        {" "}
-        Go to Sign up{" "}
-      </Link>
+        <Image source={icons.add} className="home-add-icon" />
+
+        <View className="home-balance-card">
+          <Text className="home-balance-label">Balance</Text>
+
+          <View className="home-balance-row">
+            <Text className="home-balance-amount">
+              {formatCurrency(HOME_BALANCE.amount)}
+            </Text>
+            <Text className="home-balance-date">
+              {dayjs(HOME_BALANCE.nextRenewalDate).format("MM/DD")}
+            </Text>
+          </View>
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
